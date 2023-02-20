746d6fa8533386b0787b1da83a402220e42fbb77
Jragasa <Jragasa@idealteam.com>
2023-02-20 10:24:55 -0500

    Created V2 Production Build

--- AlterDSLib/SQLQueries.cs
+++ AlterDSLib/SQLQueries.cs
@@ -8,89 +8,89 @@ namespace AlterDSLib
 {
     public abstract class SQLQueries
     {
-        protected static string GetAllItems()
-        {
-            return "SELECT * FROM [dbo].[Test_Ideal_Rerun_Log]";
-        }
         //protected static string GetAllItems()
         //{
-        //    return "SELECT * FROM [dbo].[Ideal_DS_Rerun_Log]";
+        //    return "SELECT * FROM [dbo].[Test_Ideal_Rerun_Log]";
         //}
-
-        protected static string GetAllItemsByProcessedDate(string processedDate)
+        protected static string GetAllItems()
         {
-            //return $"SELECT * FROM dbo.Ideal_DS_Rerun_Log WHERE CONVERT(VARCHAR, ProcessedDate, 120) LIKE '%{processedDate}%'";
-            return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE ProcessedDate >= '{processedDate}'";
+            return "SELECT * FROM [dbo].[Ideal_DS_Rerun_Log]";
         }
+
         //protected static string GetAllItemsByProcessedDate(string processedDate)
         //{
         //    //return $"SELECT * FROM dbo.Ideal_DS_Rerun_Log WHERE CONVERT(VARCHAR, ProcessedDate, 120) LIKE '%{processedDate}%'";
-        //    return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE ProcessedDate >= '{processedDate}'";
+        //    return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE ProcessedDate >= '{processedDate}'";
         //}
-
-        protected static string GetItemById(int id)
+        protected static string GetAllItemsByProcessedDate(string processedDate)
         {
-            return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE id = {id}";
+            //return $"SELECT * FROM dbo.Ideal_DS_Rerun_Log WHERE CONVERT(VARCHAR, ProcessedDate, 120) LIKE '%{processedDate}%'";
+            return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE ProcessedDate >= '{processedDate}'";
         }
+
         //protected static string GetItemById(int id)
         //{
-        //    return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE id = {id}";
+        //    return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE id = {id}";
         //}
-
-        protected static string GetItemByName(string name)
+        protected static string GetItemById(int id)
         {
-            return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE item = '{name}'";
+            return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE id = {id}";
         }
+
         //protected static string GetItemByName(string name)
         //{
-        //    return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE item = '{name}'";
+        //    return $"SELECT * FROM [dbo].[Test_Ideal_Rerun_Log] WHERE item = '{name}'";
         //}
-
-        protected static string InsertItem(int id, string name, string processedDate)
+        protected static string GetItemByName(string name)
         {
-            return $@"
-                SET IDENTITY_INSERT [dbo].[Test_Ideal_Rerun_Log] ON
-                INSERT INTO [dbo].[Test_Ideal_Rerun_Log] (id,item,ProcessedDate)
-                VALUES ({id}, '{name}', '{processedDate}')
-            ";
+            return $"SELECT * FROM [dbo].[Ideal_DS_Rerun_Log] WHERE item = '{name}'";
         }
+
         //protected static string InsertItem(int id, string name, string processedDate)
         //{
         //    return $@"
-        //        SET IDENTITY_INSERT [dbo].[Ideal_DS_Rerun_Log] ON
-        //        INSERT INTO [dbo].[Ideal_DS_Rerun_Log] (id,item,ProcessedDate)
+        //        SET IDENTITY_INSERT [dbo].[Test_Ideal_Rerun_Log] ON
+        //        INSERT INTO [dbo].[Test_Ideal_Rerun_Log] (id,item,ProcessedDate)
         //        VALUES ({id}, '{name}', '{processedDate}')
         //    ";
         //}
-        protected static string RemoveItemById(int id)
+        protected static string InsertItem(int id, string name, string processedDate)
         {
             return $@"
-                DELETE FROM [dbo].[Test_Ideal_Rerun_Log] WHERE id = {id}
+                SET IDENTITY_INSERT [dbo].[Ideal_DS_Rerun_Log] ON
+                INSERT INTO [dbo].[Ideal_DS_Rerun_Log] (id,item,ProcessedDate)
+                VALUES ({id}, '{name}', '{processedDate}')
             ";
         }
         //protected static string RemoveItemById(int id)
         //{
         //    return $@"
-        //        DELETE FROM [dbo].[Ideal_DS_Rerun_Log] WHERE id = {id}
+        //        DELETE FROM [dbo].[Test_Ideal_Rerun_Log] WHERE id = {id}
         //    ";
         //}
-
-        protected static string UpdateItem(int id, string name, string dateProcessed)
+        protected static string RemoveItemById(int id)
         {
             return $@"
-                UPDATE [dbo].[Test_Ideal_Rerun_Log]
-                SET item = '{name}', ProcessedDate = '{dateProcessed}'
-                WHERE id = {id}
+                DELETE FROM [dbo].[Ideal_DS_Rerun_Log] WHERE id = {id}
             ";
         }
 
         //protected static string UpdateItem(int id, string name, string dateProcessed)
         //{
         //    return $@"
-        //        UPDATE [dbo].[Ideal_DS_Rerun_Log]
+        //        UPDATE [dbo].[Test_Ideal_Rerun_Log]
         //        SET item = '{name}', ProcessedDate = '{dateProcessed}'
         //        WHERE id = {id}
         //    ";
         //}
+
+        protected static string UpdateItem(int id, string name, string dateProcessed)
+        {
+            return $@"
+                UPDATE [dbo].[Ideal_DS_Rerun_Log]
+                SET item = '{name}', ProcessedDate = '{dateProcessed}'
+                WHERE id = {id}
+            ";
+        }
     }
 }
